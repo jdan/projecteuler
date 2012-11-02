9dc176b6a58981f788ee4cbcef5364dbf0f0fcfb
Jordan Scales <scalesjordan@gmail.com>
2012-11-02 14:15:23 -0400

    using Factor module

--- haskell/Euler_3.hs
+++ haskell/Euler_3.hs
@@ -2,16 +2,7 @@
 -- 2 November 2012
 
 module Euler3 where
-
-primeFactors :: Integer -> [Integer]
-primeFactors n = primeFactors' n [] 2
-
-primeFactors' :: (Integral a) => a -> [a] -> a -> [a]
-primeFactors' 1 ls _ = ls
-primeFactors' n ls c
-  | n == c         = c:ls
-  | n `rem` c == 0 = primeFactors' (n `div` c) (c:ls) 2
-  | otherwise      = primeFactors' n ls (c + 1)
+import Factor
 
 main :: IO ()
 main = do
