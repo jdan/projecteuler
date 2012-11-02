93011f0a16cd5ac1ef758dc5313c297c494756fd
Jordan Scales <scalesjordan@gmail.com>
2012-11-02 00:01:26 -0400

    Euler 3 haskell solution

---
+++ haskell/Euler_3.hs
@@ -0,0 +1,15 @@
+module Euler3 where
+
+primeFactors :: Integer -> [Integer]
+primeFactors n = primeFactors' n [] 2
+
+primeFactors' :: (Integral a) => a -> [a] -> a -> [a]
+primeFactors' 1 ls _ = ls
+primeFactors' n ls c
+  | n == c         = c:ls
+  | n `rem` c == 0 = primeFactors' (n `div` c) (c:ls) 2
+  | otherwise      = primeFactors' n ls (c + 1)
+
+main :: IO ()
+main = do
+  print $ head $ primeFactors 600851475143
