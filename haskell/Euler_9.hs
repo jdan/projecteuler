58f1f5da89f4698e3240890f58c5362459623fbd
Jordan Scales <scalesjordan@gmail.com>
2012-11-02 13:36:23 -0400

    Euler 9 solution haskell

---
+++ haskell/Euler_9.hs
@@ -0,0 +1,10 @@
+-- Euler 9 solution by Jordan Scales
+-- 2 November 2012
+
+module Euler9 where
+
+main :: IO ()
+main = do
+  print $ head [a*b*(1000-a-b) | a <- [1000, 999..1],
+                                 b <- [a, a-1..1],
+                                 a^2 + b^2 == (1000-a-b)^2]
