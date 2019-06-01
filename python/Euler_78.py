b1ccfc39b9b95f4c6222091dc2e3cb127fd64bfb
Jordan Scales <scalesjordan@gmail.com>
2019-06-01 08:53:51 -0400

    Update Euler_78.py

--- python/Euler_78.py
+++ python/Euler_78.py
@@ -1,49 +1,38 @@
-# Project Euler 78 Solution by Jordan Scales
-# 5/18/13
-# Find the least value of n for which arrangements(n) + 1
-#   is divisible by one million.
-
-# memoize decorator
-def memoize(f):
-    if not hasattr(f, 'cache'):
-        f.cache = {}
-
-    def inner(*args):
-        if f.cache.has_key(args):
-            return f.cache[args]
-        else:
-            res = f.cache[args] = f(*args)
-            return res
-
-    return inner
-
-@memoize
-def sumways(n, lt):
-    ''' returns the number of ways you can sum `n` using numbers no
-        greater than `lt` '''
-    if n == 0:
-        # base case, return 1
-        return 1
+# https://en.wikipedia.org/wiki/Pentagonal_number
+def generalizaed_pent(n):
+    if n % 2 == 0:
+        n //= -2
     else:
-        # up until lt
-        #   compute a new sumways using a smaller total and new lt
-        #   making sure not to go into the negatives
-        return sum([sumways(n - i, i) for i in range(1, lt + 1) if n - i >= 0])
+        n = (n + 1) // 2
+        
+    return (3 * n * n - n) // 2
 
-def arrangements(n):
-    ''' returns the number of ways you can sum `n` using 2 or
-        more positive integers '''
+memo = {}
+# https://en.wikipedia.org/wiki/Partition_function_(number_theory)#Recurrence_relations
+def p(n):
+    if n <= 0: return 0
+    if n <= 1: return 1
+    if n in memo: return memo[n]
 
-    # equivalent to the ways you can sum n using numbers no
-    #   greater than n-1
-    return sumways(n, n-1)
+    i = 1
+    res = 0
+    while True:
+        j = generalizaed_pent(i)
+        if j >= n: break
 
-if __name__ == '__main__':
-    n = 300
-    while 1:
-        n += 1
-        if (arrangements(n) + 1) % 1000000 == 0:
-            break
-        print n
+        res += (1 if i % 4 == 1 or i % 4 == 2 else -1) * p(n - j)
+        i += 1
 
-    print n
+    memo[n] = res
+    return res
+
+i = 1
+res = None
+while True:
+    res = p(i)
+    if res % 1000000 == 0:
+        # Off by one error
+        print(i-1, res)
+        break
+    i += 1
+
