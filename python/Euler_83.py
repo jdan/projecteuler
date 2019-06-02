7f9e0dd61d21caf85f575ebc2215841c268c27e9
Jordan Scales <scalesjordan@gmail.com>
2019-06-01 21:51:41 -0400

    solution to problem 83

---
+++ python/Euler_83.py
@@ -0,0 +1,66 @@
+# Euler 83 Solution
+# by Jordan Scales (http://jordanscales.com)
+# 1 Jun 2019
+
+matrix = []
+f = open('matrix.txt')
+for line in f.readlines():
+    matrix.append(map(lambda s: int(s), line.split(',')))
+
+# matrix = [[131, 673, 234, 103, 18],
+#           [201, 96, 342, 965, 150],
+#           [630, 803, 746, 422, 111],
+#           [537, 699, 497, 121, 956],
+#           [805, 732, 524, 37,  331]]
+
+def graph_of_matrix(matrix):
+    graph = {}
+
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            graph[(r, c)] = {}
+
+            if c > 0:
+                graph[(r, c)][(r, c - 1)] = matrix[r][c]
+
+            if r > 0:
+                graph[(r, c)][(r - 1, c)] = matrix[r][c]
+
+            if r < len(matrix) - 1:
+                graph[(r, c)][(r + 1, c)] = matrix[r][c]
+
+            if c < len(matrix[r]) - 1:
+                graph[(r, c)][(r, c + 1)] = matrix[r][c]
+
+    return graph
+
+def dijkstra(graph, start, end):
+    Q = set([])
+    dist = {}
+
+    for v, _ in graph.items():
+        dist[v] = float('inf')
+        Q.add(v)
+
+    dist[start] = 0
+
+    while len(Q) > 0:
+        u = min(Q, key=lambda v: dist[v])
+        Q.remove(u)
+
+        if u == end:
+            return dist[end]
+        else:
+            for neighbor, _ in graph[u].items():
+                if neighbor in Q:
+                    alt = dist[u] + graph[u][neighbor]
+                    if alt < dist[neighbor]:
+                        dist[neighbor] = alt
+
+r_end = len(matrix) - 1
+c_end = len(matrix[r_end]) - 1
+
+print dijkstra(
+    graph_of_matrix(matrix),
+    (0, 0),(r_end, c_end)
+) + matrix[r_end][c_end]    # add the value of the bottom right
